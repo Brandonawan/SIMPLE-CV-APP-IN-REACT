dbdada0fb64ca8607af1dde97bbb95463e31a534
Brandon awan <abibangbrandon855@gmail.com>
2022-04-18 11:27:24 -0700

    adding forms for cv

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import Navbar from './Navbar';
+import General from './components/General';
 import Education from './components/Educational';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <div className="content">
+      <General />
       <Education />
-
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/General.js
@@ -0,0 +1,20 @@
+const General = () => {
+    return ( 
+        <div className="wrapper">
+        <h1>General Information</h1>
+        <form>
+<fieldset>
+  <label>
+    First Name:
+    <input type="text" name="name" placeholder="Enter Your first name" /> <br />
+
+    Second and Last Names
+    <input type="text" name="name" placeholder="Enter Your first name" />
+  </label>
+  </fieldset>
+</form>
+      </div>
+     );
+}
+ 
+export default General;
