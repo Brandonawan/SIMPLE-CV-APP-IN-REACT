fb39d99a5a59df36a5f897e5d663ddd31a20d267
Brandon awan <abibangbrandon855@gmail.com>
2022-04-18 09:48:16 -0700

    added components

---
+++ src/components/Educational.js
@@ -0,0 +1,18 @@
+const Education = () => {
+    return (  
+        <form>
+  <label>
+    First Name:
+    <input type="text" name="name" placeholder="Enter Your first name" /> <br />
+
+    Second Name:
+    <input type="text" name="name" placeholder="Enter Your first name" />
+  </label>
+
+</form>
+    );
+}
+
+
+ 
+export default Education;
